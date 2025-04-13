7187dfe44a4c59e66aabd4b39f88b51a795584e9
Ahmed Ibrahim Metawee' Youssef <64774266+aimtyaem@users.noreply.github.com>
2025-04-13 22:01:54 +0200

    Delete deploy.sh
    Moved to file

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-git init  # Initialize the repository (only needed once)
-git add .  # Stage all changes
-git commit -m "Updated QuantumMind IO project"
-git branch -M master  # Ensure the branch is named 'master'
-git remote add origin https://github.com/aimtyaem/RA-Space-exploration.git
-git push -u origin master
